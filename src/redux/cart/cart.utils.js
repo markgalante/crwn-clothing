62dd94955c0daa8c676703b4f584a00f6605bb19
markgalante <mark.galante.14@gmail.com>
2020-06-07 12:52:14 +0200

    Add comment for clarity

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,6 @@
 //LESSON 114; 2:00
 
-export const addItemToCart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => { //(state.cartItems, action.payload)
     const existingCartItem = cartItems.find( //returns the first value that returns true
         cartItem=> cartItem.id === cartItemToAdd.id); //does the new item match another items id? 
 
