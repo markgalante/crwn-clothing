05987b7e86fe72cc13ba0a52e1fa9189f3f817b4
markgalante <mark.galante.14@gmail.com>
2020-06-15 21:01:59 +0200

    remove redux-logger from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,8 +5,12 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer'; 
 
 //LESSON 105: Set up middleware. 
-const middlewares = [logger]; 
+const middlewares = []; 
 
+//LESSON 151 - ONLY SHOWING LOGGER IF IN PRODUCTION MODE: 
+if(process.env.NODE_ENV==="development"){
+    middlewares.push(logger);
+}
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store); //A persisted version of our store. 
