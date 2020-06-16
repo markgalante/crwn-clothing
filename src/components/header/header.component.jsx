1a84f2446ec9f8f3009ad67cac4138177f816579
markgalante <mark.galante.14@gmail.com>
2020-06-16 17:51:03 +0200

    Add styled-components to header and hompage components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react'; 
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'; //L106 
-//connect is a higher order component. 
+// import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'; //L106 //connect is a higher order component. 
 import { createStructuredSelector } from 'reselect'; 
 
 import { auth } from '../../firebase/firebase.utils'; 
@@ -12,30 +11,32 @@ import { selectCurrentUser } from '../../redux/user/user.selectors'; //121
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';  
 
-import './header.styles.scss'; 
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './headers.styles'
+
+// import './header.styles.scss'; 
 
 const Header = ({ currentUser, hidden }) => ( //L106 - now receives this from reducer
-    <div className='header'>
-        <Link className='logo-container' to='/'>
+    <HeaderContainer>
+        <LogoContainer  to='/'> {/*Removed className='logo-container'*/}
             <Logo className='logo' />
-        </Link>
+        </LogoContainer>
 
-        <div className='options'>
-            <Link to='/shop' className='option' >SHOP</Link>
-            <Link to='/contact' className='option' >CONTACT</Link>
+        <OptionsContainer>
+            <OptionLink to='/shop'>SHOP</OptionLink>
+            <OptionLink to='/contact'>CONTACT</OptionLink>
             {
                 currentUser ? 
-                <div className='option' onClick={()=> {auth.signOut()} }>SIGN OUT</div>
+                <OptionDiv onClick={()=> {auth.signOut()} }>SIGN OUT</OptionDiv>
                 :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                <OptionLink to='/signin'>SIGN IN</OptionLink>
             }
             <CartIcon /> 
-        </div>
+        </OptionsContainer>
         { 
             hidden ? null : 
             <CartDropdown />
         } 
-    </div>
+    </HeaderContainer>
 );
 
 //LESSON 106 // Edited with selector in lesson 121
