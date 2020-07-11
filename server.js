8ef75fefb473bf3102dd13648d58768004f8b85b
markgalante <mark.galante.14@gmail.com>
2020-07-11 22:34:54 +0200

    Changed server.js

--- server.js
+++ server.js
@@ -1,47 +1,45 @@
-//NODE SERVER SET UP
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path"); //allows building of paths for directories
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
-
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json()); //process all body tags to JSON. 
-app.use(bodyParser.urlencoded({ extended: true })); //url strings don't contain spaces or symbols
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client.build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    });
-};
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, error => {
-    if (error) throw error;
-    console.log("server running on port " + port);
+  if (error) throw error;
+  console.log('Server running on port ' + port);
 });
 
-app.post("/payment", (req, res) => {
-    const body = {
-        source: req.body.token.id,
-        amount: req.body.amount,
-        currency: "usd"
-    };
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
 
-    stripe.charges.create(body, (stripeErr, stripeResponse) => {
-        if (stripeErr) {
-            res.status(500).send({ error: stripeErr }); //failure status code
-        } else {
-            res.status(200).send({ success: stripeResponse });
-        }
-    }); 
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
